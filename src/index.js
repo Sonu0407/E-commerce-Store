cdb8227a857bb76920eead48e7a331d708523f3a
Sonu0407 <sonu.sapri2004@gmail.com>
2025-12-07 17:20:54 +0530

    second commit

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { CartProvider } from './CartProvider';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/E-commerce-Store">
       <CartProvider>
       <App />
       </CartProvider>
